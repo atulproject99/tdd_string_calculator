b2dcac3b41a6440ba070298134c2f81db2ff533a
atul maurya <atul@bit01.tech>
2025-06-24 12:25:44 +0530

    Added: Method signature added and handle for empty string

--- bin/string_calculator.dart
+++ bin/string_calculator.dart
@@ -1,5 +1,1 @@
-import 'package:string_calculator/string_calculator.dart' as string_calculator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${string_calculator.calculate()}!');
-}
+void main(List<String> arguments) {}
