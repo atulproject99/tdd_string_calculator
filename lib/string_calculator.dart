56c1d1e72274b6eb938bea2223815d0c0a0123dd
atul maurya <atul@bit01.tech>
2025-06-24 12:42:48 +0530

    Refactor: add method code refactored for handle new line delimiters and commas

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,4 +1,8 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  return numbers.split(',').map((e) => int.parse(e)).reduce((a, b) => a + b);
+  String delimiterPattern = '[,\n]';
+  return numbers
+      .split(RegExp(delimiterPattern))
+      .map((e) => int.parse(e))
+      .reduce((a, b) => a + b);
 }
