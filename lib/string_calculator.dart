51ca9c5dd6de6a1faccc81361c183456bbb2a67f
atul maurya <atul@bit01.tech>
2025-06-24 13:28:26 +0530

    Refactor: add method code refactored

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,18 +1,23 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
+  // Default delimiters: comma and newline
   String delimiterPattern = '[,\n]';
+
+  // Check for custom delimiter
   if (numbers.startsWith('//')) {
     final newLineIndex = numbers.indexOf('\n');
     final customDelimiterSection = numbers.substring(2, newLineIndex);
     if (customDelimiterSection.startsWith('[')) {
+      // Multiple custom delimiters like //[***][%%]
       final matches = RegExp(r'\[(.*?)\]').allMatches(customDelimiterSection);
       final escapedDelimiters =
           matches.map((e) => RegExp.escape(e.group(1)!)).toList();
       delimiterPattern = escapedDelimiters.join('|');
     } else {
+      // Single character delimiter like //;
       delimiterPattern = RegExp.escape(customDelimiterSection);
     }
-
+    // Remove the delimiter declaration from the input string
     numbers = numbers.substring(newLineIndex + 1);
   }
   final inputs =
@@ -27,5 +32,5 @@ int add(String numbers) {
     throw FormatException('negative not allowed ${negativeNoList.join(',')}');
   }
 
-  return inputs.reduce((a, b) => a + b);
+  return inputs.fold(0, (a, b) => a + b);
 }
