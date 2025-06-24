66e7e687b85038a89fb246666730a55e883020d7
atul maurya <atul@bit01.tech>
2025-06-24 12:51:29 +0530

    Test case 6 has been passed and screenshot added

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,12 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
   String delimiterPattern = '[,\n]';
+  if (numbers.startsWith('//')) {
+    final newLineIndex = numbers.indexOf('\n');
+    final customDelimiters = numbers.substring(2, newLineIndex);
+    delimiterPattern = RegExp.escape(customDelimiters);
+    numbers = numbers.substring(newLineIndex + 1);
+  }
   return numbers
       .split(RegExp(delimiterPattern))
       .map((e) => int.parse(e))
